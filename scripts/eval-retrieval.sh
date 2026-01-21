0d40140528e2e92765f900b037e56778e1d59539
Mayank Mishra <mayank31398@gmail.com>
2026-01-21 09:49:53 -0800

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/eval-retrieval.sh
+++ scripts/eval-retrieval.sh
@@ -2,6 +2,4 @@ TRITON_PRINT_AUTOTUNING=1 accelerate launch -m lm_eval \
     --model hf \
     --model_args dtype=bfloat16,pretrained=$1 \
     --tasks fda,swde,based_nq_2048,based_trivia,squad_completion,based_drop \
-    --limit 100 \
-    --metadata '{"max_seq_lengths":[2048]}' \
     --batch_size 1
