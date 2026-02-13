7c2e1fe22fddcb90f53ebdb5e23c84cfd800ae1b
Mayank Mishra <mayank31398@gmail.com>
2026-02-12 23:08:32 -0800

    lm
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/eval-lm.sh
+++ scripts/eval-lm.sh
@@ -1,5 +1,10 @@
-TRITON_PRINT_AUTOTUNING=1 accelerate launch -m lm_eval \
+export ENABLE_KERNELS=mamba2_ssm,sonicmoe,gru,rnn
+export TRITON_PRINT_AUTOTUNING=1
+export HF_HUB_OFFLINE=1
+
+accelerate launch -m lm_eval \
     --model hf \
     --model_args dtype=bfloat16,pretrained=$1 \
     --tasks wikitext,openbookqa,piqa,sciq,arc_easy,arc_challenge,boolq,copa,hellaswag,winogrande,race,lambada_openai \
-    --batch_size auto
+    --batch_size auto \
+    --output_path results-$YAML
