6ce425df9bc4e3c3bb3d5efdef8af5fd391f0d3b
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2026-02-15 14:54:31 -0800

    [SCRIPTS] update scripts (#5)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/eval-niah.sh
+++ scripts/eval-niah.sh
@@ -1,6 +1,11 @@
-TRITON_PRINT_AUTOTUNING=1 accelerate launch -m lm_eval \
+export ENABLE_KERNELS=mamba2_ssm,sonicmoe,gru,rnn
+export TRITON_PRINT_AUTOTUNING=1
+export HF_HUB_OFFLINE=1
+
+accelerate launch -m lm_eval \
     --model hf \
     --model_args dtype=bfloat16,pretrained=$1,max_length=32768 \
-    --tasks niah_single_1,niah_single_2,niah_single_3 \
-    --metadata='{"max_seq_lengths":[1024,2048,4096,8192,16384]}' \
-    --batch_size 1
+    --tasks niah_single_1,niah_single_2,niah_single_3,niah_multikey_1,niah_multiquery,niah_multivalue \
+    --metadata=metadata.json \
+    --batch_size auto \
+    --output_path $2
--- scripts/eval-retrieval.sh
+++ scripts/eval-retrieval.sh
@@ -1,5 +1,11 @@
-TRITON_PRINT_AUTOTUNING=1 accelerate launch -m lm_eval \
+export ENABLE_KERNELS=mamba2_ssm,sonicmoe,gru,rnn
+export TRITON_PRINT_AUTOTUNING=1
+export HF_HUB_OFFLINE=1
+
+accelerate launch -m lm_eval \
     --model hf \
-    --model_args dtype=bfloat16,pretrained=$1 \
+    --model_args dtype=bfloat16,pretrained=$1,max_length=32768 \
     --tasks fda,swde,based_nq_2048,based_triviaqa,squad_completion,based_drop \
-    --batch_size 1
+    --metadata=metadata.json \
+    --batch_size auto \
+    --output_path $2
