a9fcc8ce55837317f57fb898d107ad40c8aca294
Mayank Mishra <mayank31398@gmail.com>
2026-03-08 10:26:21 -0700

    fix scripts
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/eval-niah.sh
+++ scripts/eval-niah.sh
@@ -6,6 +6,6 @@ accelerate launch -m lm_eval \
     --model hf \
     --model_args dtype=bfloat16,pretrained=$1,max_length=32768 \
     --tasks niah_single_1,niah_single_2,niah_single_3,niah_multikey_1,niah_multiquery,niah_multivalue \
-    --metadata=metadata.json \
+    --metadata=sequence_lengths.json \
     --batch_size auto \
     --output_path $2
