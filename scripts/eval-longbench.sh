72f72e87bfa445a6aff5c19998ee3a0ac3915bdf
Mayank Mishra <mayank31398@gmail.com>
2026-03-08 11:13:34 -0700

    500k
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/eval-longbench.sh
+++ scripts/eval-longbench.sh
@@ -4,7 +4,7 @@ export HF_HUB_OFFLINE=1
 
 accelerate launch -m lm_eval \
     --model hf \
-    --model_args dtype=bfloat16,pretrained=$1,max_length=32768 \
+    --model_args dtype=bfloat16,pretrained=$1,max_length=500000 \
     --tasks longbench_narrativeqa,longbench_qasper,longbench_multifieldqa_en_e,longbench_hotpotqa,longbench_2wikimqa,longbench_musique,longbench_gov_report,longbench_qmsum,longbench_multi_news,longbench_trec,longbench_triviaqa,longbench_samsum,longbench_lcc,longbench_repobench-p \
     --metadata=sequence_lengths.json \
     --num_fewshot 0 \
